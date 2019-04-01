50ef62c0502919a89c5f6a928ac063e55fc9a7d3
Lepris <karolina.jankowska@gmail.com>
2019-04-01 10:48:48 +0100

    new noted added

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,9 @@
 const ENV = process.env.NODE_ENV || 'development';
-const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
+const knex = require('knex');
 
-module.exports = require('knex')(config);
+const dbConfig =
+    ENV === 'production'
+        ? { client: 'pg', connection: process.env.DATABASE_URL }
+        : require('../knexfile');
 
-// const knex = require('knex');
-// const dbConfig = require('../knexfile');
-
-// const connection = knex(dbConfig);
-
-
-// module.exports = connection;
+module.exports = knex(dbConfig);
