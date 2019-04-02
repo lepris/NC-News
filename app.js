f4473b7008e0931a6d61e03427191f510dee18f5
Lepris <karolina.jankowska@gmail.com>
2019-04-02 09:12:49 +0100

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const {
   handle200, handle400, handle500, handle404, handle422,
@@ -9,6 +10,7 @@ const bodyParser = require('body-parser');
 const apiRouter = require('./routes/apiRouter');
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.use('/api', apiRouter);
 
